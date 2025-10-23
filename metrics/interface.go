742af8f02798ac8c017c0157000e210ed03c9748
Nikolai Suvorov <jeskym3t4l@gmail.com>
2025-10-23 11:33:23 +0300

    Update interface.go

--- metrics/interface.go
+++ metrics/interface.go
@@ -1,5 +1,13 @@
-package metrics
+package stats
+
+type Snapshot interface{}
 
 type Metrics interface {
 	Snapshot() Snapshot
+}
+
+type EmptyMetrics struct{}
+
+func (e *EmptyMetrics) Snapshot() Snapshot {
+	return nil
 }
